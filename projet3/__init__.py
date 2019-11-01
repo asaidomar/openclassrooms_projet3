d2ee620282b27695a9f80403b9ba4dbb0f7dade8
Ali Said omar <saidomarali@gmail.com>
2019-11-01 16:47:49 +0100

    second

---
+++ projet3/__init__.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+# filename : __init__.py
+# date : 2019-10-23-20-19
+# project: openclassroom_projet3
+# author : alisaidomar
+
+# Selectionner un subset de colonnes
+# supprimer les doublons
+
+
+"""
+
+ 🎯Effectuer des opérations de nettoyage sur des données structurées
+
+(Correlation je ne prends que les données fournies)
+(Pour les données manquantes on peut prendre la médiane)
+❒ Les éventuelles valeurs manquantes ont été mises en évidence. Une solution est proposée (elle doit être justifiée si plusieurs choix sont possibles).
+❒ Une automatisation est réalisée :
+
+    - utiliser les fonctions et méthodes (de pandas, numpy etc) adaptées si elles existent ;
+    - éviter de copier-coller avec peu de modifications (penser aux boucles, dictionnaires).
+
+(definition doublons, les lignes exactement semblables, )
+❒ Les duplicats sont traités (de variables et d'enregistrements).
+❒ Les variables non pertinentes pour la problématique de l'app ont été éliminées.
+
+
+
+🎯Effectuer une analyse statistique univariée
+
+❒ Des métriques adaptées sont employées (moyenne ou médiane selon la dispersion).
+
+❒ Les éventuelles valeurs aberrantes ont été mises en évidence. Une solution est proposée (elle doit être justifiée si plusieurs choix sont possibles).
+
+❒ Les distributions observées sont correctement caractérisées (uni, bi, multi-modale).
+
+❒ La définition des quantiles est correctement maîtrisées. 
+
+
+
+ 🎯 Effectuer une analyse statistique multivariée
+
+❒ La définition mathématique de la corrélation et les hypothèses sous-jacentes ont été expliquées
+
+❒ Même distribution et corrélation ne sont pas confondues
+
+
+🎯Communiquer ses résultats à l’aide de représentations graphiques lisibles et pertinentes
+❒ Un graphe est utilisé à chaque fois que c'est nécessaire (ne pas décrire la distribution d'une variable sans graphe associé, par exemple !)
+
+❒ Les graphes sont lisibles (taille de texte suffisante, bonne définition)
+
+❒ Un graphique de chacun de ces types a été réalisé : boxplot, barplot, pie chart, histogramme, scatter plot
+
+
+
+🎯Respect des consignes
+
+❒ Les livrables sont complets
+
+❒ Les livrables ont été déposés 48h à l'avance
+
+❒ L'export de la base finale est présent dans le code
+
+❒ Le temps de présentation est bien géré par l'étudiant 
+
+"""
